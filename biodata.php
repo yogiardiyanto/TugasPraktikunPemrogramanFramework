7ea07a3df5e34d626b4cc6d9b6620ce2344a215d
Yogi Ardiyanto <90976232+yogiardiyanto@users.noreply.github.com>
2021-09-19 09:50:04 +0700

    Add files via upload

---
+++ biodata.php
@@ -0,0 +1,16 @@
+<?php
+$foto = "<img src = 'DSC_0032.JPG' alt=''> ";
+$nama = "Yogi Ardiyanto";
+$nim = "192520032";
+$alamat = "Silekor, Semawung Daleman RT02/RW02 Kutoarjo, Purworejo";
+$email = "yogiardi2001@gmail.com";
+$bidang_minat = "Web Design";
+
+echo "<h3 style>Biodata Diri </h3>";
+echo "".$foto. "<br>";
+echo "Nama : " .$nama. "<br>";
+echo "Nim : " .$nim. "<br>";
+echo "Alamat : " .$alamat. "<br>";
+echo "Email : " .$email. "<br>";
+echo "Bidang Minat : " .$bidang_minat. "<br>";
+?>
